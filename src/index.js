b03f25ba4c2c6ff44cd374934819921efc9df254
breno.santana <breno.santana@g.globo>
2025-06-01 14:45:12 -0300

    Ta nascendo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // Em src/index.js ou em App.jsx, no topo:
-import "../public/assets/css/reset.css";
-import "../public/assets/css/typography.css";
-import "../public/assets/css/layout.css";
-import "../public/assets/css/buttons.css";
-import "../public/assets/css/gallery.css";
-import "../public/assets/css/lists.css";
-import "../public/assets/css/utils.css";
-import "../public/assets/css/themes.css";
-import "../public/assets/css/media-queries.css";
+import ".//assets/css/reset.css";
+import ".//assets/css/typography.css";
+import ".//assets/css/layout.css";
+import ".//assets/css/buttons.css";
+import ".//assets/css/gallery.css";
+import ".//assets/css/lists.css";
+import ".//assets/css/utils.css";
+import ".//assets/css/themes.css";
+import ".//assets/css/media-queries.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./language-toggle"; // script que define window.setLanguage, etc.
+import ".//assets/js/language-toggle"; // script que define window.setLanguage, etc.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
